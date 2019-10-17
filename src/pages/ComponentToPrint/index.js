8102130903a52783a5508a1f64f7ee3f72bf251b
PierreBosch <pierre.moota@gmail.com>
2019-10-17 08:50:09 -0300

    Initial commit

---
+++ src/pages/ComponentToPrint/index.js
@@ -0,0 +1,33 @@
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
+
+export default class ComponentToPrint extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <th>column 1</th>
+          <th>column 2</th>
+          <th>column 3</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+          <tr>
+            <td>data 1</td>
+            <td>data 2</td>
+            <td>data 3</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable flowtype/require-valid-file-annotation */
+
+import React, { Component } from "react";
+import Example from "./Example";
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default Index;
